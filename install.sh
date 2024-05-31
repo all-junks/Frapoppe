33941aaab84037b0f83b6e51dcd47ec650ec8128
Ishan-Git <88433029+all-junks@users.noreply.github.com>
2024-05-31 10:34:58 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,37 @@
 
 sudo unzip -q Frapoppe-theme.zip -d /usr/share/themes/
 
+PS3="Select your desktop env.: "
+
+select de in gnome other
+do
+    case $de in 
+        "gnome")
+            echo "Continuing..."
+            break;;
+        "other")
+            exit 0
+    esac
+done
+
 PS3="Select your distro: "
 
 select os in deb arch rhel suse
 do
     case $os in
         "deb")
-          sudo apt install gnome-tweaks;;
+            sudo apt install gnome-tweak
+            break;;
         "arch")
-          sudo pacman -S gnome-tweaks;;
+            sudo pacman -S gnome-tweaks
+            break;;
         "rhel")
-          sudo dnf install gnome-tweaks;;
+            sudo dnf install gnome-tweaks
+            break;;
         "suse")
-          sudo zypper install gnome-tweaks;;
+            sudo zypper install gnome-tweaks
+            break;;
         *)
-          echo "Invalid option. Retrying..."
+            echo "Invalid option. Retrying..."
     esac
 done
