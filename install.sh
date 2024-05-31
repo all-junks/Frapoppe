0090860a447475a5aa4a609565faa97025260a0d
Ishan-Git <88433029+all-junks@users.noreply.github.com>
2024-05-31 10:21:09 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 sudo unzip -q Frapoppe-theme.zip -d /usr/share/themes/
+
+PS3="Select your distro: "
+
+select os in deb arch rhel suse
+do
+    case $os in
+        "deb")
+          sudo apt install gnome-tweaks;;
+        "arch")
+          sudo pacman -S gnome-tweaks;;
+        "rhel")
+          sudo dnf install gnome-tweaks;;
+        "suse")
+          sudo zypper install gnome-tweaks;;
+        *)
+          echo "Invalid option. Retrying..."
+    esac
+done
